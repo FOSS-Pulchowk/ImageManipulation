83a9c666915772749e9f8388f9032b05b5b719b6
Safal <074bct531.safal@pcampus.edu.np>
2018-08-09 23:23:15 +0545

    added passport file

---
+++ passpport.py
@@ -0,0 +1,29 @@
+# this script can make a 1200x1800px photo
+# with the required passport size photos
+# that can be printed
+
+import os
+import sys
+from PIL import Image
+
+os.chdir('E:\\KADA\python_learn\project24\ImageManipulation')
+filename = sys.argv[1]
+bg_im = Image.new('RGB', (1200, 1800), 'white')
+width, height = bg_im.size
+os.makedirs('ppsize', exist_ok=True)
+
+if not (filename.endswith('.png') or filename.endswith('.jpg')):
+    print('Please give a valid im file')
+
+im = Image.open(filename)
+im = im.resize((350, 400))
+imWidth, imHeight = im.size
+
+print('Creating your photo...')
+
+for left in range(37, width - (imWidth + 37), (imWidth + 37)):
+    for top in range(35, height - imHeight, (imHeight + 35)):
+        bg_im.paste(im, (left, top))
+
+bg_im.save(os.path.join('ppsize', filename))
+print('image succesfully created!')
